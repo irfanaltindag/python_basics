cebce42faee320399e8b9d92a336d61689af7cfe
Irfan Altindag <altiirf@hotmail.com>
2022-09-29 00:10:37 +0200

    Quiz Game1

--- HelloWorld/myFirstProject.py
+++ HelloWorld/myFirstProject.py
@@ -1,1 +1,33 @@
-print("my name is irfan")
+print("Welcome to my quiz!")
+
+playing = input("Do you want to play? ")
+
+if playing.lower() != "yes":
+    print("see you later!")
+    quit()
+else:
+    print("Okay let´s go! :)")
+
+answer = input("Wer hat dieses Spiel programmiert? ")
+if answer.lower() == "irfan":
+    print("Richtig! :)")
+else:
+    print("LEIDER FALSCH :(")
+
+answer = input("Wie heißt seine Frau? ")
+if answer.lower() == "rana":
+    print("Richtig! :)")
+else:
+    print("LEIDER FALSCH :(")
+
+answer = input("Wie heißen seine eltern? ")
+if answer.lower() == "fatma und olcay":
+    print("Richtig! :)")
+else:
+    print("LEIDER FALSCH :(")
+
+answer = input("Wie heißen seine Geschwister? ")
+if answer.lower() == "emirhan, azra und zümra":
+    print("DU HAST DAS SPIEL DURCHGESPIELT! GRATULATION! :)")
+else:
+    print("LEIDER FALSCH :(")
